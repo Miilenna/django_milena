9ca797ac64b16dc13b999a118efbcc175115275f
Miilenna <milena200.fv@gmail.com>
2025-01-23 16:43:39 +0100

    arreglado y funciona

--- ACTIVITAT_13/TIC_BCN_MVA/TIC_BCN_MVA/settings.py
+++ ACTIVITAT_13/TIC_BCN_MVA/TIC_BCN_MVA/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'centre.apps.CentreConfig'
+    'centre',
 ]
 
 MIDDLEWARE = [
--- ACTIVITAT_13/TIC_BCN_MVA/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('prova/', include('prova.urls')),
-    path('admin/', admin.site.urls),
-]
