f971f2ca506283aeef8282198b55235e3c6aeaed
Miilenna <milena200.fv@gmail.com>
2025-02-01 19:53:32 +0100

    activitat 1 acabada

--- ACTIVITAT_13/TIC_BCN_MVA/centre/views.py
+++ ACTIVITAT_13/TIC_BCN_MVA/centre/views.py
@@ -9,15 +9,122 @@ from django.template import loader
     # return HttpResponse(template)
 
 def profe(request):
-    professor = {"name":"Roger", "surname1":"Sobrino", "surname2":"Gil", "correu":"roger.sobrino@iticbcn.cat", "curs":"DAW2A", "tutor":True, "moduls": "M7"}
-    template = loader.get_template('profesor.html')
-    dades = template.render({'nombre':professor["name"], 'surname1':professor["surname1"], 'surname2':professor["surname2"], 'correu':professor["correu"],
-                             'curs':professor["curs"], 'tutor':professor["tutor"], 'moduls':professor["moduls"]})
-    return HttpResponse(dades)
+    professor = {
+        'profe1': {
+            "name":"Roger",
+            "surname1":"Sobrino",
+            "surname2":"Gil",
+            "correu":"roger.sobrino@iticbcn.cat",
+            "curs":["DAW2A", "DAM2B"],
+            "tutor":True,
+            "moduls": "M7"
+        },
+        'profe2':{
+            "name":"Josep Oriol",
+            "surname1": "Roca",
+            "surname2": "Fabra",
+            "correu": "joseporiol.roca@iticbcn.cat",
+            "curs": ["DAW2B", "DAW2A", "DAW1A"],
+            "tutor":True,
+            "moduls": "M8"
+        },
+        'profe3': {
+            "name": "Juan Manuel",
+            "surname1": "Sánchez",
+            "surname2": "Bel",
+            "correu": "juanmanuel.sanchez@iticbcn.cat",
+            "curs": ["DAW2B", "DAW2A"],
+            "tutor": True,
+            "moduls": "M6"
+        }
+
+    }
+    context = {"profes": professor}
+    return render(request, 'profesor.html', context)
 
 def alumn(request):
-    alumnat = {"name":"Milena", "surname1":"Vardumyan", "surname2":"Aleksanyan", "correu":"2023_milena.vardumyan@iticbcn.cat", "curs":"DAW2A", "moduls": ["M7", "M6", "M5"]}
-    template = loader.get_template('students.html')
-    dades = template.render({'nombre':alumnat["name"], 'surname1':alumnat["surname1"], 'surname2':alumnat["surname2"], 'correu':alumnat["correu"],
-                             'curs':alumnat["curs"], 'moduls':alumnat["moduls"]})
-    return HttpResponse(dades)
+    alumnat = {
+        'alumne1': {
+            "name": "Hugo",
+            "surname1": "Sansegundo",
+            "surname2": "Costantini",
+            "correu": "2023_hugo.sansegundo@iticbcn.cat",
+            "curs": "DAW2A",
+            "moduls": ["M06", "M07", "M08", "ML"]
+        },
+        'alumne2': {
+            "name": "Adrián",
+            "surname1": "Navarro",
+            "surname2": "Pérez",
+            "correu": "2023_adrian.navarro@iticbcn.cat",
+            "curs": "DAW2A",
+            "moduls": ["M06", "M07", "M08", "Cloud"]
+        },
+        'alumne3': {
+            "name": "Xavi",
+            "surname1": "Porras",
+            "surname2": "del Pino",
+            "correu": "2023_xavier.porras@iticbcn.cat",
+            "curs": "DAW2A",
+            "moduls": ["M06", "M07", "M08"]
+        },
+        'alumne4': {
+            "name": "Javier",
+            "surname1": "Giménez",
+            "surname2": "Sánchez",
+            "correu": "2023_javier.gimenez@iticbcn.cat",
+            "curs": "DAW2A",
+            "moduls": ["M7", "M6", "M8"]
+        },
+        'alumne5': {
+            "name": "Milena",
+            "surname1": "Vardumyan",
+            "surname2": "Aleksanyan",
+            "correu": "2023_milena.vardumyan@iticbcn.cat",
+            "curs": "DAW2A",
+            "moduls": ["M7", "M6", "M013", "Big Data"]
+        },
+        'alumne6': {
+            "name": "Daniel",
+            "surname1": "Vallespin",
+            "surname2": "Mellado",
+            "correu": "2023_daniel.vallespin@iticbcn.cat",
+            "curs": "DAW2A",
+            "moduls": ["M7", "M6", "M8"]
+        },
+        'alumne7': {
+            "name": "Félix Bequet",
+            "surname1": "Balbin",
+            "surname2": "Silva",
+            "correu": "2023_felix.balbin@iticbcn.cat",
+            "curs": "DAW2A",
+            "moduls": ["M7", "M6", "M8", "Big Data"]
+        },
+        'alumne8': {
+            "name": "Victor Andrés",
+            "surname1": "Fernández",
+            "surname2": "Álvarez",
+            "correu": "2023_victor.fernandez@iticbcn.cat",
+            "curs": "DAW2A",
+            "moduls": ["M7", "M6", "M8", "IA"]
+        },
+        'alumne9': {
+            "name": "Lila",
+            "surname1": "Díez",
+            "surname2": "Zhou",
+            "correu": "2023_lila.diez@iticbcn.cat",
+            "curs": "DAW2A",
+            "moduls": ["M7", "M6", "M8", "ML"]
+        },
+        'alumne10': {
+            "name": "Arman",
+            "surname1": "Mohammed",
+            "surname2": "Akther",
+            "correu": "2023_arman.mohammed@iticbcn.cat",
+            "curs": "DAW2A",
+            "moduls": ["M7", "M6", "M8"]
+        }
+
+    }
+    context = {"alumn":alumnat}
+    return render(request, 'students.html', context)
--- README.md
+++ README.md
@@ -1,9 +1,14 @@
 # django_milena
 
 ### Actividad 1:
-Página principal de la aplicación de teachers y estudiantes:
-![alumnos](ACTIVITAT_13/img/alumnes.png)
-![profesors](ACTIVITAT_13/img/profesors.png)
-Página principal del proyecto:
+Página de teachers y estudiantes desde la aplicación:
+![alumn.png](ACTIVITAT_13/img/alumn.png)
+![profe.png](ACTIVITAT_13/img/profe.png)
+
+Página de teachers y estudiantes desde el proyecto:
+![alumnat_header.png](ACTIVITAT_13/img/alumnat_header.png)
+![profe_header.png](ACTIVITAT_13/img/profe_header.png)
+
+### Actividad 2:
 
 
