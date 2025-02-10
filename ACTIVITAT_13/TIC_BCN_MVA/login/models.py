60bdb8b37be86454266dfd3e76db1790badb2d66
Miilenna <milena200.fv@gmail.com>
2025-02-10 18:35:08 +0100

    commit classe

--- ACTIVITAT_13/TIC_BCN_MVA/login/models.py
+++ ACTIVITAT_13/TIC_BCN_MVA/login/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Person:
+    id = models.AutoField(primary_key=True)
+    email = models.CharField(unique=True)
+    nom = models.CharField(max_length=100)
+    cognom = models.CharField(max_length=100)
+    cognom2 = models.CharField(max_length=100)
+
