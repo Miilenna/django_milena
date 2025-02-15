9e457494bb21b0f611985ef9e26140e4e2f785fd
Miilenna <milena200.fv@gmail.com>
2025-02-15 16:12:14 +0100

    formulari de login

--- ACTIVITAT_13/TIC_BCN_MVA/login/urls.py
+++ ACTIVITAT_13/TIC_BCN_MVA/login/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
     path('user-form/', views.user_form, name='user_form'),
-    path('inici/', views.inici, name='inici')
+    path('', views.inici, name='inici')
 ]
