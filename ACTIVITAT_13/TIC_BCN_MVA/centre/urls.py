0d4148970ca31cbaa83c6f0fb6a845f00635ceb8
Miilenna <milena200.fv@gmail.com>
2025-02-16 16:39:30 +0100

    errors de la app de centre arreglats

--- ACTIVITAT_13/TIC_BCN_MVA/centre/urls.py
+++ ACTIVITAT_13/TIC_BCN_MVA/centre/urls.py
@@ -8,9 +8,7 @@ urlpatterns = [
     path('students/', views.alumn, name='alumn'),
 
     path('profes/profe/<str:pk>/', views.profe_datos, name='profe_datos'),
-    path('profes/', views.index_profe, name='index_profe')
+    path('profes/', views.alumn_profe, name='index_profe'),
 
-    path('', views.index, name='index'),
-    path('profes_alumnes/<str:pk>/', views.alumn_profe, name='alumn_profe')
-    path('', views.index, name='index')
+    path('profes_alumnes/', views.alumn_profe, name='alumn_profe'),
 ]
