d32b4d2e33bf084ff6c5d49bf7d1317fd6219ee3
Miilenna <milena200.fv@gmail.com>
2025-02-16 18:45:26 +0100

    vistes de login sense session

--- ACTIVITAT_13/TIC_BCN_MVA/login/views.py
+++ ACTIVITAT_13/TIC_BCN_MVA/login/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import PersonForm
+from django.contrib.auth.models import User
+from django.contrib.auth import login as auth_login
+
 # Create your views here.
 def inici(request):
     return render(request, 'inici.html')
@@ -8,4 +11,30 @@ def user_form(request):
     form = PersonForm()
     context = {'form':form}
     return render(request, 'form.html', context)
+
+
+def login_form(request):
+    return render(request, 'login.html')
+
+def process_login(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        contrasenya = request.POST.get('contrasenya')
+
+        try:
+            user = User.objects.get(email=email)
+
+            if user.check_password(contrasenya):
+                auth_login(request, user)
+                return redirect('inici')
+            else:
+                return redirect('login_error')
+
+        except User.DoesNotExist:
+            return redirect('login_error')
+
+    return redirect('login_form')
+
+def login_error(request):
+    return render(request, 'login.html', {'error': 'Credenciales incorrectas'})
 
